d0de0031077aea03fb771fd83ec12d28d0ca1cf3
John Kraal <jpakraal@gmail.com>
2013-01-20 14:00:06 +0100

    better password security

--- src/cms/cms/model/cmsuser.py
+++ src/cms/cms/model/cmsuser.py
@@ -13,14 +13,10 @@ class CMSUser(Base):
     id = Column(Integer, primary_key=True)
     username = Column(String(50), index=True)
     password = Column(String(64))
-    local_seed = Column(String(16))
     fullname = Column(String(255))
 
     def __repr__(self):
-        return "<CMSUser id=%d;username=%s>" % (self.id, self.username)
+        return "<CMSUser id={0};username={1}>".format(self.id, self.username)
 
     def passwdMatch(self, passwd):
-        return (self.password == security.hashed_passwd(passwd,
-                                                        self.local_seed,
-                                                        config.get('pwd_seed'))
-                )
+        return security.check_password(passwd, self.password)
