3c5f15071af31d6dbd46364f8adaa63fa07d0db1
John Kraal <jpakraal@gmail.com>
2013-01-20 13:52:46 +0100

    Changed salt size to 12 iterations as 512 is a bit much

--- src/cms/cms/lib/security.py
+++ src/cms/cms/lib/security.py
@@ -2,7 +2,7 @@ import bcrypt
 
 
 def hash_password(password):
-    return bcrypt.hashpw(password, bcrypt.gensalt(512))
+    return bcrypt.hashpw(password, bcrypt.gensalt(12))
 
 
 def check_password(password, stored_password):
